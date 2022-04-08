ad40b569576150c9116700ccbaec8712fb48b50f
vgabrielk <vfelix1553@gmail.com>
2022-04-08 01:46:35 -0300

    modified

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,24 +1,24 @@
 
 <template>
   <div v-if="addedTask" class="success">
-    <h5>Task added with success </h5>
-    <p> ✔️ </p>
+    <h5>Adicionado com sucesso</h5>
+    <p><i class="fa-solid fa-check"></i></p>
   </div>
   <div class="tasks">
     <div class="row">
-    <h1>Tasks</h1>
-    <input
-    placeholder="Type to add a new task"
-      maxlength="22"
-      type="text"
-      @keypress.enter="inputSubmit"
-      v-model="currentTask"
-    />
-    <button @click="addTask">Add a new task</button>
+      <h1>Lista de tarefas</h1>
+      <input
+        placeholder="Escreva para adicionar uma nova tarefa!"
+        maxlength="22"
+        type="text"
+        @keypress.enter="inputSubmit"
+        v-model="currentTask"
+      />
+      <button @click="addTask">Adicionar uma tarefa</button>
     </div>
-    <div class="tasks-content">
-      <div v-for="(task, index) in tasks" :key="index">
-        <ul @click="addBackground">
+    <div class="border" v-for="(task, index) in tasks" :key="index">
+      <div class="tasks-content">
+        <ul>
           <li>{{ task.title }}</li>
           <span @click="removeTask(task)">&times;</span>
         </ul>
@@ -29,13 +29,12 @@
 
 <script>
 export default {
-
   data() {
     return {
       name: "Tasks",
       currentTask: "",
       tasks: [],
-      addedTask : false
+      addedTask: false,
     };
   },
   methods: {
@@ -44,14 +43,12 @@ export default {
         this.tasks.push({
           title: this.currentTask,
         });
-        this.currentTask = "",
-          this.addedTask = true
+        (this.currentTask = ""), (this.addedTask = true);
       }
-          setTimeout(() =>{
-      this.addedTask = false
-    },1000)
+      setTimeout(() => {
+        this.addedTask = false;
+      }, 2000);
     },
-
 
     inputSubmit() {
       this.addTask();
@@ -59,7 +56,6 @@ export default {
     removeTask(task) {
       this.tasks = this.tasks.filter((t) => t.title != task.title);
     },
-
   },
 };
 </script>
@@ -70,7 +66,18 @@ export default {
   flex-direction: column;
   padding: 5rem;
   justify-content: center;
-  color:#31f500
+  color: #31f500;
+}
+.border {
+  height: auto;
+  width: auto;
+  border: 1px solid #333;
+  border-radius: 12px;
+  margin: .2rem 0;
+  padding: 1rem;
+}
+.tasks h1 {
+  color: #fafafa;
 }
 input {
   width: 250px;
@@ -81,42 +88,31 @@ input {
   box-shadow: 0px 0px 18px rgba(0, 0, 0, 0.585);
   color: #31f500;
 }
-input::-webkit-input-placeholder{
+input::-webkit-input-placeholder {
   color: #fafafa;
-  font-size: .680rem;
+  font-size: 0.68rem;
 }
 button {
-  margin-top: 1rem;
+  margin: 1rem 0;
   background: transparent;
   box-shadow: 0px 0px 18px rgba(0, 0, 0, 0.585);
   border-radius: 10px;
   padding: 10px;
   cursor: pointer;
-  color: #31f500;
+  color: #fafafa;
   transition: 0.5s ease;
   width: 20%;
   font-weight: bold;
-  transform: scale(.9);
+  transform: scale(0.9);
 }
 button:hover {
   transform: translateY(-5px);
 }
-@media (max-width: 768px) {
-  input,
-  button {
-    width: 100%;
-  }
-  .tasks {
-    align-items: center;
-    padding: 2rem;
-  }
-}
+
 ul {
-  margin-top: 1rem;
   background: transparent;
   box-shadow: 0px 0px 18px rgba(0, 0, 0, 0.585);
-  min-width: auto;
-  width: auto;
+  width: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -129,7 +125,7 @@ ul {
 }
 span {
   position: absolute;
-  right: -1rem;
+  right: -2.8rem;
   background: #222;
   box-shadow: 4px 0px 18px black;
   display: flex;
@@ -141,17 +137,13 @@ span {
   font-size: 1.5rem;
   font-weight: bold;
   color: rgb(172, 172, 172);
+  padding: 0 1rem;
 }
 .tasks-content {
   width: 200px;
 }
 
-.isdone {
-  background: transparent;
-  position: absolute;
-  right: -2rem;
-}
-.success{
+.success {
   position: fixed;
   left: 2rem;
   top: 1rem;
@@ -163,7 +155,39 @@ span {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  transition: .5s ease;
+  transition: 0.5s ease;
   z-index: 100;
+  animation: notification 0.3s linear;
+}
+.success p {
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  border: 2px solid #31f500;
+  display: grid;
+  place-items: center;
+  color: #31f500;
+}
+@keyframes notification {
+  from {
+    transform: translateY(-50px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+}
+@media (max-width: 768px) {
+  input,
+  button {
+    width: 100%;
+  }
+  .tasks {
+    align-items: center;
+    padding: 2rem;
+  }
+  .tasks h1 {
+    text-align: center;
+  }
+
 }
 </style>
